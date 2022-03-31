b9ca9961cd70c04eb6334afbdcae52e7d8a1b108
Scott Fillmer <scottfillmer@me.com>
2022-03-31 14:33:21 -0500

    Add files

---
+++ years.js
@@ -0,0 +1,24 @@
+// my age
+let myAge = 51;
+
+// first two years of a dog's life
+let earlyYears = 2
+
+earlyYears *= 10.5;
+
+// later dog years
+let laterYears = myAge - 2;
+
+// number of dog years
+laterYears *= 4;
+
+// check both variables
+// console.log(earlyYears, laterYears);
+
+// my age in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+
+// my name
+let myName = 'Scott Fillmer'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
